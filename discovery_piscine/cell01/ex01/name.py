3bf6c7f12ae358d49d77462bff302a9b8f5d140c
asiaapipoo <asiaapipoo@gmail.com>
2026-09-21 23:59:45 +0700

    commit 3

---
+++ discovery_piscine/cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name: str = "Apipoo"
+last_name: str = "Meearj"
+whole_name: str = f"{first_name} {last_name}"
+print(whole_name)
---
+++ discovery_piscine/cell01/ex02/age.py
@@ -0,0 +1,4 @@
+age: int = 20
+increment: int = 42
+my_age: int = age + increment
+print(my_age)
