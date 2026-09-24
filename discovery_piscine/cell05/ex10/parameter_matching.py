15340ee840eb0f03645b465626895ce6c7210f5e
asiaapipoo <asiaapipoo@gmail.com>
2026-09-24 12:11:09 +0700

    commit 12

--- discovery_piscine/cell05/ex10/parameter_matching.py
+++ discovery_piscine/cell05/ex10/parameter_matching.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python3
 import sys
 
 if len(sys.argv) != 2:
     print("none")
 else:
     param = sys.argv[1]
-    text = input() 
+    text = input("What was the parameter? ")
     if param == text:
         print("Good job!")
     else:
--- discovery_piscine/cell05/ex13/append_it.py
+++ discovery_piscine/cell05/ex13/append_it.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
-
 import sys
 
-[print(s if s.endswith("ism") else s + "ism") for s in sys.argv[1:]] or print("none" if len(sys.argv) < 2 else "", end="\n")
+args = sys.argv[1:]
+
+if not args:
+    print("none")
+else:
+    for s in args:
+        if not s.endswith("ism"):
+            print(s + "ism")
